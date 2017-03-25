4ddca40574cdb3b7cd15d40e19dae92aec325c1f
jimmyfarrell <jfarrell22@gmail.com>
2017-03-25 19:51:32 -0400

    More deploy prep

--- server/index.js
+++ server/index.js
@@ -3,25 +3,33 @@ const path = require('path');
 const webpack = require('webpack');
 
 const app = express();
-const webpackConfig = require('../webpack.config.dev');
-const compiler = webpack(webpackConfig);
+let port = 8080;
 
-app.use(require('webpack-dev-middleware')(compiler, {
-  noInfo: true,
-  publicPath: webpackConfig.output.publicPath
-}));
+if (process.env.NODE_ENV === 'production') {
+  app.use('/static', express.static('dist'))
+} else {
+  const webpackConfig = require('../webpack.config.dev');
+  const compiler = webpack(webpackConfig);
 
-app.use(require('webpack-hot-middleware')(compiler));
+  port = 3000;
+
+  app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: webpackConfig.output.publicPath
+  }));
+
+  app.use(require('webpack-hot-middleware')(compiler));
+}
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client', 'index.html'));
 });
 
-app.listen(3000, 'localhost', (err) => {
+app.listen(port, 'localhost', (err) => {
   if (err) {
     console.log(err);
     return;
   }
 
-  console.log('Listening on port 3000');
+  console.log(`Listening on port ${port}`);
 });
