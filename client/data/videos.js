88a0790a07f3bdb61c5a44562a45c00d33f8580c
jimmyfarrell <jfarrell22@gmail.com>
2017-03-26 08:11:57 -0400

    Add more win game videos

--- client/data/videos.js
+++ client/data/videos.js
@@ -1,5 +1,7 @@
 const videos = [
-  'https://www.youtube.com/embed/qux8CbHPlgw'
+  'https://www.youtube.com/embed/qux8CbHPlgw',
+  'https://www.youtube.com/embed/783EsrHchXA',
+  'https://www.youtube.com/embed/7V2rrbhwCLg'
 ];
 
 export default videos;
