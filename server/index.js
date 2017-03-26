fee25d3ae86e764fa91de63d8d73174cbd1d99b3
jimmyfarrell <jfarrell22@gmail.com>
2017-03-25 21:21:49 -0400

    Remove locahost

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client', 'index.html'));
 });
 
-app.listen(port, 'localhost', (err) => {
+app.listen(port, (err) => {
   if (err) {
     console.log(err);
     return;
