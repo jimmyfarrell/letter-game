f91955768d565f39f12efb0c328b6f4e250e7dcb
jimmyfarrell <jfarrell22@gmail.com>
2017-03-26 14:18:33 -0400

    Remove nice-job audio

--- client/data/sounds.js
+++ client/data/sounds.js
@@ -7,7 +7,6 @@ const gameSounds = {
   incorrect: 'https://s3.amazonaws.com/letter-game/audio/game/try-again.wav',
   get correct() {
     return sample([
-      'https://s3.amazonaws.com/letter-game/audio/game/nice-job.wav',
       'https://s3.amazonaws.com/letter-game/audio/game/applause.mp3',
       'https://s3.amazonaws.com/letter-game/audio/game/yay.wav'
     ]);
